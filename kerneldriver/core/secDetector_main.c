260d754399ea5fba8a20b4e081c94e1426b88f03
zcfsite <zhchf2010@126.com>
2023-09-21 16:26:10 +0800

    add secDetector_main.c secDetector_manager.h

---
+++ kerneldriver/core/secDetector_main.c
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2023 Huawei Technologies Co., Ltd. All rights reserved.
+ * secDetector is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ *
+ * Author: zcfsite
+ * create: 2023-09-21
+ * Description: secDetector main entry.
+ */
+
+
+#include <linux/module.h>
+#include <linux/proc_fs.h>
+#include "secDetector_manager.h"
+#include "response_unit/secDetector_proc.h"
+
+struct proc_dir_entry *g_root_dir;
+
+static unsigned int log_size = 4096;
+module_param(log_size, unit, 0400);
+MODULE_PARM_DESC(log_size, "log size");
+
+static int __init secDetector_init(void)
+{
+	int ret;
+	secDetector_init_manager();
+	g_root_dir = proc_mkdir_mode("secDetector", 0500, NULL);
+	if (g_root_dir == NULL) {
+		pr_err("[secDetector] make proc dir failed\n");
+		return -EFAULT;
+	}
+
+	ret = secDetector_init_log(g_root_dir, log_size);
+	if (ret != 0) {
+		pr_err("[secDetector] init log failed\n");
+		proc_remove(g_root_dir);
+		return ret;
+	}
+
+	pr_debug("[secDetector] init success\n");
+	return 0;
+}
+
+static void __exit secDetector_exit(void)
+{
+	secDector_destroy_log();
+	proc_remove(g_root_dir);
+
+	pr_debug("[secDetector] exit success\n");
+}
+
+module_init(secDetector_init);
+module_exit(secDetector_exit);
+MODULE_LICENSE("GPL");
