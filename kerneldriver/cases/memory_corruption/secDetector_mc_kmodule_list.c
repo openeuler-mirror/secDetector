816943877053b0b9ec335db240bf0331bb6155d3
yieux <yangxy79315@sina.com>
2023-10-13 15:52:22 +0800

    内存修改探针-隐藏内核模块识别 的响应机制补充调用 response_report

--- kerneldriver/cases/memory_corruption/secDetector_mc_kmodule_list.c
+++ kerneldriver/cases/memory_corruption/secDetector_mc_kmodule_list.c
@@ -9,20 +9,24 @@
 #include <linux/list.h>
 #include <linux/spinlock.h>
 #include "secDetector_mc_kmodule_list.h"
+#include "secDetector_response.h"
+#include <linux/slab.h>
 
 #define MODULE_LIST_MAXSIZE 0x10000
+#define MC_KMODULE_REPORT_WORD_LEN 55
 
 // 3 ways for get kernel module list.
 // struct module->list
 // struct module->mkobj->kobj->entry
 // struct module->mkobj->kobj->kset
-
 void check_kmodule_list(void)
 {
 	struct module_kobject *mobj = NULL;
 	struct kobject *k = NULL;
 	struct module *m = NULL;
 	struct kset *module_kset = __this_module.mkobj.kobj.kset;
+	response_data_t log;
+
 
 	if (module_kset == NULL)
 	return;
@@ -37,8 +41,14 @@ void check_kmodule_list(void)
 
 		mutex_lock(&module_mutex);
 		m = find_module(k->name);
-		if (m == NULL)
+		if (m == NULL) {
 			pr_err("[secDetector] mc kmoudle list find! module_name=%s.\n", k->name);
+			log.report_data.len = MC_KMODULE_REPORT_WORD_LEN + strlen(k->name);
+			log.report_data.text = kmalloc(log.report_data.len, GFP_KERNEL);
+			sprintf(log.report_data.text, "[secDetector] mc kmoudle list find! module_name=%s.\n", k->name);
+			secdetector_report(&log);
+			kfree(log.report_data.text);
+		}
 		mutex_unlock(&module_mutex);
 	}
 	spin_unlock(&module_kset->list_lock);
